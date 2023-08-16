6ab4880fb48c276c31e375c3ed71a3187c3cc697
Ewa Szymańska <ewa.szymanskax@gmail.com>
2023-08-16 19:41:13 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: {
+    relative: true,
+    files:["./src/**/*.{html,js}", "./src/*.{html,js}"],
+  },
+  theme: {
+    colors: {
+      'beige': '#DCC1AB',
+      'gray': '#F5F0EC', 
+      'green': '#1B5B31',
+    },
+    fontFamily: {
+      'montserrat': ['Montserrat'],
+      'lato': ['Lato'],
+      'garamond': ['Garamond']
+  },
+    extend: { 
+    },
+  },
+  plugins: [],
+}
+
