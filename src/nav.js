c46a7fd7622ea4073a23dd8a837b7e8cb7c9bafa
Ewa Szymańska <ewa.szymanskax@gmail.com>
2023-08-18 18:29:33 +0200

    desktop style

---
+++ src/nav.js
@@ -0,0 +1,17 @@
+const search = document.getElementById("search")
+const input = document.getElementById("input")
+const closeButton = document.getElementById("closeButton")
+
+const searching=()=>{
+    input.style.display= "inline";
+    closeButton.style.display="inline";
+}
+
+const closeInput=()=>{
+    input.style.display="none";
+    closeButton.style.display="none";
+}
+search.addEventListener("click",searching),
+closeButton.addEventListener("click", closeInput)
+
+
